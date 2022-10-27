cec0e4678411d120df5a503005f655fb50c0d2e2
Adrià Rojo <arojo@pasiona.com>
2022-10-27 12:01:42 +0200

    initial commit

---
+++ SocialGames.TechnicalTest.Api/Controllers/GamesControllers.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SocialGames.TechnicalTest.Api.Controllers
+{
+    [ApiController]
+    public class GamesControllers : ControllerBase
+    {
+        [Route("api/games/{gameId}/play")]
+        [HttpPost]
+        public async Task<IActionResult> Play(string gameId)
+        {
+            return Ok(gameId);
+        }
+    }
+}
